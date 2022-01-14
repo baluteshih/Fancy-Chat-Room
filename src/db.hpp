13bd15d72cf3e6b215985b0e7245072a931056b5
baluteshih <brian369999@gmail.com>
2022-01-14 12:03:45 +0800

    Modify spec of db.hpp

--- src/db.hpp
+++ src/db.hpp
@@ -1,11 +1,15 @@
 #include <string>
-#include <vector>
+#include "server_object.hpp"
 
 class DataBase {
     /*
     For the objects, all of the items should be well-filled.
     The only exception is creating data. 
     The "id" can be arbitrary since the database will modify it.
+    
+    about return value:
+    0 always means successful
+    Return <0 value when failure. Different values represent different kinds of errors 
     */
     class Table_User {
     public:
@@ -14,14 +18,17 @@ class DataBase {
         /* 
         return 0 if successfully added, and modify user.userid
         return -1 if the username already exists 
-        return -2 if other failure
         */
-        int login(User &user);
+        int update_password(int userid, std::string password);
+        int add_friend(int userid, int friendid);
         /*
-        return 0 if successfully login
-        return -1 if failure
+        retunr -1 if the user already has a friend with id "friendid"
         */
-        User get_object(int userid);
+        int delete_friend(int user_id, int friendid);
+        /*
+        retunr -1 if there is no friend with id "friendid"
+        */
+        User get_object(int user_id);
         int get_id(std::string username);
     };
     class Table_Message {
@@ -30,9 +37,14 @@ class DataBase {
         int create_message(Message &message);
         /*
         return 0 if successfully added, and modify message.message_id
-        return -1 if failure
         */
         Message get_object(int message_id);
+        int get_id(int chatroom_id, int sequence_id);
+        std::vector<Message> query_range_fixed_chatroom(int chatroom_id, int lower, int upper);
+        /*
+        return vector of Message which ids between [lower, upper]
+        */
+        int maxseqid_of_chatroom(int chatroom_id);
     };
     class Table_Chatroom {
     public:
@@ -40,7 +52,14 @@ class DataBase {
         int create_chatroom(Chatroom &chatroom);
         /*
         return 0 if successfully added, and modify chatroom.chatroom_id
-        return -1 if failure
+        */
+        int add_user(int chatroom_id, int user_id);
+        /*
+        retunr -1 if the user already in the chatroom
+        */
+        int delete_user(int chatroom_id, int user_id);
+        /*
+        retunr -1 if the user not in the chatroom
         */
         Chatroom get_object(int chatroom_id);
     };
--- src/server_object.hpp
+++ src/server_object.hpp
@@ -1,7 +1,12 @@
 #include <string>
+#include <vector>
+
+/*
+zero based id!!
+*/
 
 class User {
-    User(std::string _name, std::string _pass, int _id){}
+    User(std::string _name, std::string _pass, std::vector<int> _list, int _id = 0){}
     std::string username;
     /*
     a unique name
@@ -10,14 +15,15 @@ class User {
     /*
     a hashed password
     */
-    int userid;
+    std::vector<int> friend_idlist;
+    int user_id;
     /*
-    a unique id, maintain by the database
+    a sequentially unique id, maintain by the database
     */
 };
 
 class Message {
-    Message(int _cid, int _sid, long long _time, std::string _txt, std::string _hash, int _type, int _mid){}
+    Message(int _cid, int _sid, long long _time, std::string _txt, std::string _hash, int _type, int _seqid = 0, int _mid = 0){}
     int chatroom_id;
     int sender_id;
     long long timestamp;
@@ -35,17 +41,21 @@ class Message {
     type 0: string message
     type 1: file
     */
+    int sequence_id;
+    /*
+    the order of the message in its chatroom (sort by message_id)
+    */
     int message_id;
     /*
-    a unique id, maintain by the database
+    a sequentially unique id, maintain by the database
     */
 };
 
 class Chatroom {
-    Chatroom(std::vector<User> _list, int _id = 0){}
-    std::vector<User> userlist;
+    Chatroom(std::vector<int> _list, int _id = 0){}
+    std::vector<int> user_idlist;
     int chatroom_id;
     /*
-    a unique id, maintain by the database
+    a sequentially unique id, maintain by the database
     */
 };
