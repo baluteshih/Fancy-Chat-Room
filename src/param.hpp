ba1517b52bab08fa4efa414c59ce0322984e17c8
baluteshih <brian369999@gmail.com>
2022-01-14 17:18:31 +0800

    Add param.hpp/BUFSIZE

--- src/param.hpp
+++ src/param.hpp
@@ -1,3 +1,6 @@
 #include <string>
+#include <unistd.h>
 
 const std::string DBNAME                = "chatroom.db";
+const int         MAXFD                 = getdtablesize();
+const int         BUFSIZE               = 65536;
