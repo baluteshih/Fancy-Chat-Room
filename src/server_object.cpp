f231de5cc5460f9f62f2c222522506b6f42ddcaa
baluteshih <brian369999@gmail.com>
2022-01-15 10:32:11 +0800

    Fix compile error of server_object.o

--- src/server_object.cpp
+++ src/server_object.cpp
@@ -1,12 +1,12 @@
 #include "server_object.hpp"
 
-User::User(std::string _name, std::string _pass, std::vector<int> _list, int _id = 0) :
+User::User(std::string _name, std::string _pass, std::vector<int> _list, int _id) :
     username(_name),
     password(_pass),
     friend_idlist(_list),
     user_id(_id){}
 
-Message::Message(int _cid, int _sid, long long _time, std::string _txt, std::string _hash, int _type, int _seqid = 0, int _mid = 0) :
+Message::Message(int _cid, int _sid, long long _time, std::string _txt, std::string _hash, int _type, int _seqid, int _mid) :
     chatroom_id(_cid),
     sender_id(_sid),
     timestamp(_time),
@@ -16,6 +16,6 @@ Message::Message(int _cid, int _sid, long long _time, std::string _txt, std::str
     sequence_id(_seqid),
     message_id(_mid) {}
 
-Chatroom::Chatroom(std::vector<int> _list, int _id = 0) :
+Chatroom::Chatroom(std::vector<int> _list, int _id) :
     user_idlist(_list),
     chatroom_id(_id) {}
