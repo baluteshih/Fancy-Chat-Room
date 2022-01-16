a73ff8ed312244faaf253b84e08af794ce94a6de
Chris Chien (Built with Travis-CI) <chris.89357@gmail.com>
2022-01-16 19:31:10 +0800

    Add URL Decoder

---
+++ src/url.cpp
@@ -0,0 +1,25 @@
+#include "url.hpp"
+std::string urldecode(std::string raw){
+    char p[2] = {0};
+    int n = (int)raw.size();
+    std::string res;
+    for (int i = 0, j = 0; i < n; i++){
+        p[0] = p[1] = 0;
+        if (raw[i] == '+'){
+            res += ' ';
+            continue;
+        }
+        if (raw[i] != '%'){
+            res += raw[i];
+            continue;
+        }
+        p[0] = raw[++i];
+        p[1] = raw[++i];
+        std::function<void(char&)> magic = [&](char& c){
+            c = c - 48 - (c >= 'A' ? 7 : 0) - (c >= 'a' ? 32 : 0);
+        };
+        magic(p[0]); magic(p[1]);
+        res += (unsigned char)((p[0] << 4) + p[1]);
+    }
+    return res;
+}
---
+++ src/url.hpp
@@ -0,0 +1,5 @@
+#ifndef _URL_HPP_
+#define _URL_HPP_
+#include <string>
+std::string urldecode(std::string raw);
+#endif // _URL_HPP_
