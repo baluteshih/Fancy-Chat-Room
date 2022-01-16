b4b20121d627e84413d54383d32653c3e9e3ea58
baluteshih <brian369999@gmail.com>
2022-01-16 10:52:34 +0800

    update html.cpp

--- src/html.cpp
+++ src/html.cpp
@@ -1,4 +1,5 @@
 #include "html.hpp"
+#include "file.hpp"
 #include <map>
 std::string p(std::string sentence){
     return "<p>" + sentence + "</p>";
@@ -16,7 +17,7 @@ std::string homepage(std::string username, std::string addr_manage_friend, std::
     val["addr_list_chatroom"] = addr_list_chatroom;
     val["addr_change_password"] = addr_change_password;
     val["addr_homepage"] = addr_homepage;
-    const std::string filename("../public_server/html/homepage.html");
+    const std::string filename(path_combine(std::vector<std::string>({SERVER_PUBLIC_DIR, "html", "homepage.html"})));
     std::ifstream input_file(filename);
     std::string ans;
     char c;
@@ -45,7 +46,7 @@ std::string homepage(std::string username, std::string addr_manage_friend, std::
     return ans;
 }
 std::string manage_friend(std::vector<std::string> friend_name, std::string form_action, std::string addr_homepage){
-    const std::string filename("../public_server/html/manage_friend.html");
+    const std::string filename(path_combine(std::vector<std::string>({SERVER_PUBLIC_DIR, "html", "manage_friend.html"})));
     std::ifstream input_file(filename);
     std::string ans;
     char c;
@@ -72,7 +73,7 @@ std::string manage_chatroom(std::vector<std::string> chatroom_name, std::vector<
     if ((int)chatroom_name.size() != (int)href_name.size()){
         _helper_fail("#chatroom != #href");
     }
-    const std::string filename("../public_server/html/manage_chatroom.html");
+    const std::string filename(path_combine(std::vector<std::string>({SERVER_PUBLIC_DIR, "html", "manage_chatroom.html"})));
     std::ifstream input_file(filename);
     std::string ans;
     char c;
@@ -100,7 +101,7 @@ std::string manage_chatroom(std::vector<std::string> chatroom_name, std::vector<
     return ans;
 }
 std::string chat_page(std::string chatroom_name, std::vector<std::string> message, std::string prev_href, std::string next_href, std::string post_action, std::string addr_homepage){
-    const std::string filename("../public_server/html/chat_page.html");
+    const std::string filename(path_combine(std::vector<std::string>({SERVER_PUBLIC_DIR, "html", "chat_page.html"})));
     std::ifstream input_file(filename);
     std::string ans;
     char c;
@@ -131,7 +132,7 @@ std::string chat_page(std::string chatroom_name, std::vector<std::string> messag
     return ans;
 }
 std::string update_password(std::string change_action, std::string addr_homepage){
-    const std::string filename("../public_server/html/update_password.html");
+    const std::string filename(path_combine(std::vector<std::string>({SERVER_PUBLIC_DIR, "html", "update_password.html"})));
     std::ifstream input_file(filename);
     std::string ans;
     char c;
